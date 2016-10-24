0ba26c0dd4b200dd8a45e9ed4732524234a83bd1
nekfeu <kevin.empociello@epitech.eu>
2016-10-24 09:35:00 +0200

    Init repo with reviews and design

--- Realife/ViewController.swift
+++ Realife/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var viewStar: UIView!
+    var screenSize = UIScreen.main.bounds
+    var star: StarReview!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        star = StarReview(frame: CGRect(x: 40, y: 0, width: screenSize.width - 80, height: 80))
+        star.starMarginScale = 0.3
+        star.allowEdit = true
+        star.allowAccruteStars = false
+        star.minimunValue = -1
+        star.starFillColor = UIColor.orange
+        star.starBackgroundColor = UIColor.white
+        star.addTarget(self, action: "test", for: UIControlEvents.valueChanged)
+        viewStar.addSubview(star)
+        
+        profilePicture.layer.cornerRadius = 50
+        profilePicture.layer.borderWidth = 2
+        profilePicture.layer.borderColor = UIColor.white.cgColor
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func test() {
+        print(star.value)
     }
-
-
 }
 
